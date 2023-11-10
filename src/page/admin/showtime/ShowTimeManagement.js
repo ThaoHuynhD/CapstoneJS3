49a4cfb9495eed81ea662d82d06ce5b6fa8d68d9
ThaoHuynhD <thaohuynh34.678@gmail.com>
2023-11-10 10:21:54 +0700

    clean code show manahement

--- src/page/admin/showtime/ShowTimeManagement.js
+++ src/page/admin/showtime/ShowTimeManagement.js
@@ -1,68 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Form, Input, Select, message, DatePicker, TimePicker, Modal } from 'antd';
-import { getMovieDetail, getMovieList, getTheaterByTheaterGroup, getTheaterGroup } from '../../../api/mainApi';
-import { getShowTimeCreate } from '../../../api/adminApi';
+import { Button, Form, Select, message, Modal } from 'antd';
+import { getMovieDetail, getMovieList } from '../../../api/mainApi';
 import ShowTimeListByMovie from '../../guest/MovieDetailPage/ShowTimeListByMovie';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import dayjs from 'dayjs';
-import moment from 'moment';
 import MovieInformation from './MovieInformation'
-
-
+import AddShowTime from './AddShowTime';
 
 export default function ShowTimeManagement({ selectedMaPhim }) {
     const [movieList, setMovieList] = useState([]);
     const [movieSelected, setMovieSelected] = useState(selectedMaPhim !== null ? selectedMaPhim : '13189');
-    const [theaterGroupList, setTheaterGroupList] = useState([]);
-    const [theaterList, setTheaterList] = useState([]);
-
-    const [theaterGroupSelected, setTheaterGroupSelected] = useState(null);
-    const [theaterSelected, setTheaterSelected] = useState(null);
-
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const [form] = Form.useForm();
-    const formItemLayout = {
-        labelCol: { xs: { span: 24, }, sm: { span: 8, }, },
-        wrapperCol: { xs: { span: 24, }, sm: { span: 16, }, },
-    };
     dayjs.extend(customParseFormat);
 
 
-    const [myvalues, setMyvalues] = React.useState(
-        {
-            maHeThongRap: null,
-            maCumRap: null,
-            ngayChieu: null,
-            gioChieu: null,
-            giaVe: null,
-        }
-    );
-    const setFieldMaCumRap = () => {
-        setMyvalues({ maCumRap: null, });
-        if (theaterGroupSelected !== null) { form.setFieldsValue(myvalues); }
-    };
-
-    const movieArr = [];
-    const theaterGroupArr = [];
-    const theaterArr = [];
-
-    //gán giá trị cho các state
     const handleMovieSelection = (movieSelected) => {
         setMovieSelected(movieSelected);
-        setTheaterGroupSelected(null);
-        setTheaterSelected(null);
-    };
-    const handleTheaterGroupSelection = (theaterGroupSelected) => {
-        setTheaterGroupSelected(theaterGroupSelected);
-        setTheaterSelected(null);
-        setFieldMaCumRap();
-    };
-    const handleTheaterSelection = (theaterSelected) => {
-        setTheaterSelected(theaterSelected);
     };
 
-    let fetchDataMovieList = async () => {
+    const fetchDataMovieList = async () => {
         try {
             let response = await getMovieList();
             setMovieList(response.data.content);
@@ -71,34 +29,7 @@ export default function ShowTimeManagement({ selectedMaPhim }) {
         }
     };
 
-    let fetchDataTheaterGroupList = async () => {
-        try {
-            let response = await getTheaterGroup();
-            setTheaterGroupList(response.data.content);
-        } catch {
-            message.error("Đã có lỗi xảy ra");
-        }
-    };
-
-    let fetchDataTheaterList = async (theaterGroupSelected) => {
-        if (theaterGroupSelected === null) return;
-        try {
-            let response = await getTheaterByTheaterGroup(theaterGroupSelected);
-            setTheaterList(response.data.content);
-        } catch {
-            message.error("Đã có lỗi xảy ra");
-        }
-    };
-    useEffect(() => {
-        fetchDataMovieList();
-        fetchDataTheaterGroupList();
-    }, []);
-    useEffect(() => {
-        fetchDataTheaterList(theaterGroupSelected)
-    }, [theaterGroupSelected]);
-
-    // Cập nhật các danh sách
-
+    const movieArr = [];
     movieList.forEach((movie) => {
         const movieUpdate = {
             value: movie.maPhim,
@@ -106,43 +37,6 @@ export default function ShowTimeManagement({ selectedMaPhim }) {
         }
         movieArr.push(movieUpdate);
     })
-
-    theaterGroupList.forEach((theaterGroup) => {
-        const TheaterGroupUpdate = {
-            value: theaterGroup.maHeThongRap,
-            label: theaterGroup.tenHeThongRap,
-        }
-        theaterGroupArr.push(TheaterGroupUpdate);
-
-        theaterList.forEach((theater) => {
-            if (theaterGroup.maHeThongRap === theaterGroupSelected) {
-                const theaterUpdate = {
-                    value: theater.maCumRap,
-                    label: theater.tenCumRap,
-                }
-                theaterArr.push(theaterUpdate);
-            }
-        })
-    })
-
-
-    const handleAddShowTime = async (values) => {
-        let ngayChieuGioChieu = moment(values.ngayChieu).format('DD-MM-YYYY') + ' ' + moment(values.gioChieu).format('HH:mm:ss')
-        let showTimeUpdate = {
-            maPhim: movieSelected,
-            maRap: theaterSelected,
-            ngayChieuGioChieu: ngayChieuGioChieu,
-            giaVe: values.giaVe,
-        }
-        try {
-            await getShowTimeCreate(showTimeUpdate);
-            message.success("Tạo Lịch Chiếu thành công");
-            hiddenModal();
-        } catch (error) {
-            message.error("Đã có lỗi xảy ra");
-            console.log(error);
-        }
-    };
 
     const fetchDataMovieInfo = async (maPhim) => {
         try {
@@ -174,11 +68,10 @@ export default function ShowTimeManagement({ selectedMaPhim }) {
         setIsModalOpen(true);
     };
 
-
     const hiddenModal = () => { setIsModalOpen(false); };
-
-
-
+    useEffect(() => {
+        fetchDataMovieList();
+    }, []);
     return (
         <div>
             <div className='grid grid-cols-2'>
@@ -201,82 +94,10 @@ export default function ShowTimeManagement({ selectedMaPhim }) {
             <Modal title="Tạo Lịch Chiếu Mới" open={isModalOpen} onOk={showModal}
                 onCancel={hiddenModal} width={900} footer={null}
             >
-                <Form className='mx-auto my-5 border p-5'
-                    {...formItemLayout}
+                <AddShowTime
                     form={form}
-                    name="register"
-                    onFinish={handleAddShowTime}
-                    style={{
-                        maxWidth: 1200,
-                    }}
-                    scrollToFirstError
-                    initialValues={{
-                        maHeThongRap: theaterGroupSelected === null ? 'Chọn Hệ Thống Rạp' : '1',
-                        maCumRap: theaterSelected === null ? 'Chọn Cụm Rạp' : '2'
-                    }}
-                >
-                    <Form.Item name="maHeThongRap" label="Mã Hệ Thống Rạp">
-                        <Select
-                            options={theaterGroupArr}
-                            onChange={handleTheaterGroupSelection}
-                        />
-                    </Form.Item>
-                    <Form.Item name="maCumRap" label="Mã Cụm Rạp" shouldUpdate>
-                        <Select
-                            options={theaterArr}
-                            onChange={handleTheaterSelection}
-                            disabled={theaterGroupSelected === null}
-                        />
-                    </Form.Item>
-                    <div className='flex flex-auto'>
-                        <div className='w-32 mr-2'></div>
-                        <Form.Item name="ngayChieu"
-                            label="Ngày Chiếu"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Vui lòng nhập ngày Chiếu!',
-                                },
-                            ]}
-                        >
-                            <DatePicker
-                                size="large"
-                                format="YYYY-MM-DD"
-                                className='mx-2'
-                            />
-                        </Form.Item>
-                        <Form.Item name="gioChieu"
-                            label="Giờ Chiếu"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Vui lòng nhập giờ Chiếu!',
-                                },
-                            ]}
-                        >
-                            <TimePicker
-                                size="large"
-                                format="HH:mm"
-                            />
-                        </Form.Item>
-                    </div>
-                    <Form.Item name="giaVe"
-                        label="Giá Vé"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Vui lòng nhập giá vé!',
-                                whitespace: true,
-                            },
-                        ]}>
-                        <Input />
-                    </Form.Item>
-                    <div className='text-center'>
-                        <Button className='btn btn-red' htmlType="submit">
-                            Tạo Lịch Chiếu
-                        </Button>
-                    </div>
-                </Form>
+                    setIsModalOpen={setIsModalOpen}
+                    movieSelected={movieSelected} />
             </Modal>
         </div>
     )
